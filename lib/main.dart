d5a3f14fe21754413aa73f2fe9b2040f8ef6a18f
Mayank Gupta <ubuntu568@gmail.com>
2022-03-01 10:56:09 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SmartHomeApp());
+}
+
+class SmartHomeApp extends StatelessWidget {
+  const SmartHomeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}

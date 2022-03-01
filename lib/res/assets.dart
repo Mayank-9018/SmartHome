db3c451e9af1092c7269df5b621b1e7b00fe4478
Mayank Gupta <ubuntu568@gmail.com>
2022-03-01 16:15:28 +0530

    Add assets

---
+++ lib/res/assets.dart
@@ -0,0 +1,9 @@
+class AssetImages {
+  static const String bathroom = "assets/bathtub.svg";
+  static const String balcony = "assets/balcony.svg";
+  static const String bedroom = "assets/bed.svg";
+  static const String outdoor = "assets/house.svg";
+  static const String kitchen = "assets/kitchen.svg";
+  static const String living = "assets/room.svg";
+  static const String defaultRoom = "assets/default.svg";
+}

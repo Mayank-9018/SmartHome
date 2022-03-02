847145c1dd4223f3ded916c6350b9941e92931df
Mayank Gupta <ubuntu568@gmail.com>
2022-03-02 15:15:07 +0530

    Use Strech as OverScrollIndicator

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:smart_home/res/light_theme.dart';
 import 'package:smart_home/screens/control_panel.dart';
 
 void main() {
@@ -10,8 +11,9 @@ class SmartHomeApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ControlPanelScreen(),
+    return MaterialApp(
+      theme: lightTheme,
+      home: const ControlPanelScreen(),
     );
   }
 }
---
+++ lib/res/light_theme.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+  androidOverscrollIndicator: AndroidOverscrollIndicator.stretch,
+);

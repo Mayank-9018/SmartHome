daff0c9a3e792351c9c01cbe4860a8d003d8dbd2
Mayank Gupta <ubuntu568@gmail.com>
2022-03-04 19:08:46 +0530

    Update data

--- lib/models/light.dart
+++ lib/models/light.dart
@@ -4,5 +4,6 @@ class Light {
   final String name;
   double brightness;
   Color color;
-  Light(this.name, this.brightness, this.color);
+  bool supportsColors;
+  Light(this.name, this.brightness, this.color, {this.supportsColors = false});
 }
--- lib/res/data.dart
+++ lib/res/data.dart
@@ -2,22 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:smart_home/models/room.dart';
 import 'package:smart_home/res/assets.dart';
 
+import '../models/light.dart';
+
 final List<Room> rooms = [
   Room(
     name: 'Bedroom',
     lights: 2,
     illustration: AssetImages.bedroom,
     lightsData: [
-      {
-        "name": "Nightstand",
-        "brightness": 1.0,
-        "color": Colors.amber,
-      },
-      {
-        "name": "Ceiling",
-        "brightness": 1.0,
-        "color": Colors.white,
-      },
+      Light(
+        "Nightstand",
+        0.0,
+        Colors.amber,
+        supportsColors: true,
+      ),
+      Light(
+        "Main lights",
+        1.0,
+        Colors.white,
+      ),
     ],
   ),
   Room(
@@ -25,16 +28,16 @@ final List<Room> rooms = [
     lights: 2,
     illustration: AssetImages.living,
     lightsData: [
-      {
-        "name": "Ceiling",
-        "brightness": 1.0,
-        "color": Colors.white,
-      },
-      {
-        "name": "Chandelier",
-        "brightness": 0.5,
-        "color": Colors.amber,
-      },
+      Light(
+        "Ceiling",
+        1.0,
+        Colors.white,
+      ),
+      Light(
+        "Chandelier",
+        0.25,
+        Colors.amber,
+      ),
     ],
   ),
   Room(
@@ -42,16 +45,17 @@ final List<Room> rooms = [
     lights: 2,
     illustration: AssetImages.kitchen,
     lightsData: [
-      {
-        "name": "Ceiling",
-        "brightness": 0.0,
-        "color": Colors.white,
-      },
-      {
-        "name": "Countertop track lights",
-        "brightness": 1.0,
-        "color": Colors.white,
-      },
+      Light(
+        "LEDs",
+        0.0,
+        Colors.white,
+        supportsColors: true,
+      ),
+      Light(
+        "Countertop track lights",
+        1.0,
+        Colors.white,
+      ),
     ],
   ),
   Room(
@@ -59,11 +63,11 @@ final List<Room> rooms = [
     lights: 1,
     illustration: AssetImages.bathroom,
     lightsData: [
-      {
-        "name": "LEDs",
-        "brightness": 1.0,
-        "color": Colors.white,
-      },
+      Light(
+        "LEDs",
+        1.0,
+        Colors.white,
+      ),
     ],
   ),
   Room(
@@ -71,21 +75,23 @@ final List<Room> rooms = [
     lights: 3,
     illustration: AssetImages.outdoor,
     lightsData: [
-      {
-        "name": "Garage",
-        "brightness": 0.0,
-        "color": Colors.white,
-      },
-      {
-        "name": "Front Door",
-        "brightness": 0.5,
-        "color": Colors.amber,
-      },
-      {
-        "name": "Boundary",
-        "brightness": 0.2,
-        "color": Colors.amber,
-      },
+      Light(
+        "Garage",
+        0.0,
+        Colors.white,
+        supportsColors: true,
+      ),
+      Light(
+        "Front Door",
+        0.5,
+        Colors.white,
+        supportsColors: true,
+      ),
+      Light(
+        "Boundary",
+        0.2,
+        Colors.amber,
+      ),
     ],
   ),
   Room(
@@ -93,16 +99,17 @@ final List<Room> rooms = [
     lights: 2,
     illustration: AssetImages.balcony,
     lightsData: [
-      {
-        "name": "String lights",
-        "brightness": 0.0,
-        "color": Colors.amber,
-      },
-      {
-        "name": "Wall light",
-        "brightness": 0.7,
-        "color": Colors.amber,
-      },
+      Light(
+        "String lights",
+        0.0,
+        Colors.red,
+      ),
+      Light(
+        "Wall light",
+        0.7,
+        Colors.amber,
+        supportsColors: true,
+      ),
     ],
   ),
 ];
